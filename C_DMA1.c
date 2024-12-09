0dcf46a4a74ceb1f69e1079a3637bf9ab836a659
SchaeferAnjon <168791945+SchaeferAnjon@users.noreply.github.com>
2024-12-09 20:54:43 +0100

    学完了动态分配内存.

---
+++ C_DMA1.c
@@ -0,0 +1,14 @@
+//calloc 分配空间 然后初始化. 
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+    //calloc 可以分配内存并且数据的初始化. 
+    //但是在实际中一般用malloc 效率跟高. 
+    
+    int *p = calloc(100*sizeof(int),sizeof(int));
+
+
+
+
+}
